a4c561317cc89fe94b6e63f7bb014ad770766aab
yukiryo <1196484477@qq.com>
2025-12-10 20:29:11 +0800

    fix: Optimize iOS PWA update strategy with workbox config

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,11 @@ export default defineConfig({
                         purpose: 'any maskable'
                     }
                 ]
+            },
+            workbox: {
+                cleanupOutdatedCaches: true,
+                skipWaiting: true,
+                clientsClaim: true
             }
         })
     ]
